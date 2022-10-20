b813b607ff2401438f34d996aad6fa8ffb42e116
浅田清文 <kiyofumi.a.0122@gmail.com>
2022-10-20 16:15:18 +0900

    read topPage from model

--- venv_app/py_sns_project/py_sns_app/views.py
+++ venv_app/py_sns_project/py_sns_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+#models
+from .models import Post
 
 # Create your views here.
 
 # root(/)
 def topPage(request):
   print("==========views:TopPage==========")
-  return render(request,"top.html")
+  # read
+  read = Post.objects.all()
+  return render(request, "top.html", {"posts": read})
